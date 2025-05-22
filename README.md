beceadc0b0ea30c304a6d431672fe907ccd0a527
Madeline <mdlnecg@gmail.com>
2025-05-22 20:10:34 +0700

    Experiment 3.2: Be Creative!

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 Saya mencoba untuk menjalani Websocket dan Yewchat dengan command `npm start`. Kemudian, akan muncul halaman untuk memasukkan username dan tombol untuk memulai percakapan. Di sini, saya mencoba untuk membuka dua tab di awal agar terdapat lebih dari satu user yang melakukan percakapan.
 ![alt text](img/image.png)
 
+
+## Add Some Creativities to the Webclient
+Saya mencoba untuk mengubah tampilan halaman agar memiliki nuansa berwarna ungu dan mengubah tata letak bubble chat sesuai sudut pandang user.
+![alt text](img/image-2.png)
--- YewChat/src/components/chat.rs
+++ YewChat/src/components/chat.rs
@@ -163,26 +163,42 @@ impl Component for Chat {
                     }
                 </div>
                 <div class="grow h-screen flex flex-col">
-                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
+                    <div class="w-full h-14 border-b-2 border-purple-300 bg-purple-100">
+                        <div class="text-xl p-3 text-purple-800">{"💬 Chat!"}</div>
+                    </div>
                     <div class="w-full grow overflow-auto border-b-2 border-gray-300">
                         {
                             self.messages.iter().map(|m| {
-                                let user = self.users.iter().find(|u| u.name == m.from).unwrap();
-                                html!{
-                                    <div class="flex items-end w-3/6 bg-gray-100 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg ">
-                                        <img class="w-8 h-8 rounded-full m-3" src={user.avatar.clone()} alt="avatar"/>
-                                        <div class="p-3">
-                                            <div class="text-sm">
-                                                {m.from.clone()}
-                                            </div>
-                                            <div class="text-xs text-gray-500">
+                                let is_self = m.from == *ctx.link().context::<User>(Callback::noop()).unwrap().0.username.borrow();
+                                let alignment = if is_self { "justify-end" } else { "justify-start" };
+                                let bubble_color = if is_self { "bg-purple-500 text-white" } else { "bg-gray-100 text-black" };
+                                let border_radius = if is_self {
+                                    "rounded-tl-lg rounded-tr-lg rounded-bl-lg"
+                                } else {
+                                    "rounded-tl-lg rounded-tr-lg rounded-br-lg"
+                                };
+
+                                let user = self.users.iter().find(|u| u.name == m.from);
+                                let avatar = user.map(|u| u.avatar.clone()).unwrap_or_default();
+
+                                html! {
+                                    <div class={classes!("w-full", "flex", alignment, "px-4", "py-2")}>
+                                        if !is_self {
+                                            <img class="w-8 h-8 rounded-full mr-2" src={avatar.clone()} alt="avatar"/>
+                                        }
+                                        <div class={classes!("max-w-xs", bubble_color, "p-3", border_radius)}>
+                                            <div class="text-sm font-semibold">{ &m.from }</div>
+                                            <div class="text-sm break-words">
                                                 if m.message.ends_with(".gif") {
-                                                    <img class="mt-3" src={m.message.clone()}/>
+                                                    <img class="mt-2 rounded-lg" src={m.message.clone()}/>
                                                 } else {
-                                                    {m.message.clone()}
+                                                    { &m.message }
                                                 }
                                             </div>
                                         </div>
+                                        if is_self {
+                                            <img class="w-8 h-8 rounded-full ml-2" src={avatar.clone()} alt="avatar"/>
+                                        }
                                     </div>
                                 }
                             }).collect::<Html>()
@@ -191,7 +207,7 @@ impl Component for Chat {
                     </div>
                     <div class="w-full h-14 flex px-3 items-center">
                         <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                        <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
+                        <button onclick={submit} class="p-3 shadow-sm bg-purple-600 hover:bg-purple-700 w-10 h-10 rounded-full flex justify-center items-center text-white">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                             </svg>
